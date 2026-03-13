10f4172d311d5d361067075cc602b5a721347ac7
Linofff <rowerman2137@gmail.com>
2026-03-13 13:25:43 +0100

    laptok config initial commit

--- modules/system/hardware.nix
+++ modules/system/hardware.nix
@@ -7,48 +7,26 @@
       powerOnBoot = true;
     };
 
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-
-    nvidia = {
-      modesetting.enable = true;
-
-      powerManagement = {
-        enable = false;
-        finegrained = false;
-      };
-
-      open = true;
-      nvidiaSettings = false;
-    };
-
     opentabletdriver.enable = true;
     uinput.enable = true;
   };
-
   services = {
-    xserver.videoDrivers = [ "nvidia" ];
+
+    xserver.videoDrivers = [ "modesetting" ];
 
     tlp = {
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-        #Optional helps save long term battery health
-        # START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-        # STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-
+        CPU_MAX_PERF_ON_BAT = 75;
       };
     };
   };
