84ce2268396ae819ed1cfa762187d9fe3e7a1c2c
Linofff <rowerman2137@gmail.com>
2026-03-20 18:35:57 +0100

    new power settings

--- hosts/acer/hardware/power-management.nix
+++ hosts/acer/hardware/power-management.nix
@@ -12,11 +12,19 @@
     tlp = {
       enable = true;
       settings = {
+				CPU_BOOST_ON_AC = 1;
+				CPU_BOOST_ON_BAT = 0;
+				CPU_HWP_DYN_BOOST_ON_AC = 1;
+				CPU_HWP_DYN_BOOST_ON_BAT = 1;
+
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power"; # balanced, performance, low-power
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
