61c09fde1f5a1ab9f8ff8dadff54fd5aaf61c2e7
Linofff <rowerman2137@gmail.com>
2026-03-20 17:55:04 +0100

    prime

--- hosts/lenovo/hardware/hardware.nix
+++ hosts/lenovo/hardware/hardware.nix
@@ -22,6 +22,13 @@
 
       open = true;
       nvidiaSettings = false;
+
+			prime = {
+				offload.enable = true;
+
+				intelBusId = "PCI:0:2:0";
+				nvidiaBusId = "PCI:1:0:0";
+			};
     };
 
     opentabletdriver.enable = true;
