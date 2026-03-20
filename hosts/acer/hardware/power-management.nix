7b618dd123b2dc4acffe8af27e84bf272a0afebd
Linofff <rowerman2137@gmail.com>
2026-03-20 13:43:49 +0100

    new power settings

--- hosts/acer/hardware/power-management.nix
+++ hosts/acer/hardware/power-management.nix
@@ -21,7 +21,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 75;
+        CPU_MAX_PERF_ON_BAT = 60;
       };
     };
   };
